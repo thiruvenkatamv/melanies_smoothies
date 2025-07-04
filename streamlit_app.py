2d27d17a9e96568732eead27418e9f123e51f91b
thiruvenkatamv <vthiruvenkatam@gmail.com>
2025-07-04 12:12:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,13 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 # Write directly to the app
-st.title(f" My Parents New Healthy Diner ")
-st.write(" **Breakfast Menu** ")
-st.write("Omega 3 & Blueberry Oatmeal Kale")
-st.write("Spinach & Rocket Smoothie")
-st.write("Hard-Boiled Free-Range Egg")
-st.stop()
+#st.title(f" My Parents New Healthy Diner ")
+#st.write(" **Breakfast Menu** ")
+#st.write("Omega 3 & Blueberry Oatmeal Kale")
+#st.write("Omega 3 & Blueberry Oatmeal Kale")
+#st.write("Spinach & Rocket Smoothie")
+#st.write("Hard-Boiled Free-Range Egg")
+#st.stop()
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write(
