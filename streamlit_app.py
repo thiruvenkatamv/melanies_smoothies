5296e041fd69147dc32183e10119808a8118e51e
thiruvenkatamv <vthiruvenkatam@gmail.com>
2025-07-04 12:45:24 +0530

    Update streamlit_app.py
    Runtime FRUIT Name settings

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ if ingredients_list:
     ingredients_string = ''
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(each_fruit +" Nutrition Information")
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+each_fruit)
         sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
